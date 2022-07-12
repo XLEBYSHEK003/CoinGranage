db69553dbfa16da4615f9335b61723cae0e55bab
Klarulor <55636688+Klarulor@users.noreply.github.com>
2022-07-12 15:01:32 +0300

    Space? For what?

--- EventHandler.cs
+++ EventHandler.cs
@@ -6,7 +6,6 @@ namespace CoinGranageEXILED
 {
     internal class EventHandler
     {
-
         public void OnFlippingCoin(FlippingCoinEventArgs ev) 
         {
             ev.Player.ThrowGrenade(GrenadeType.FragGrenade, true);
